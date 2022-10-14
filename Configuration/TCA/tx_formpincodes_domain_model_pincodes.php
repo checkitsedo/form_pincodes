6b94f0b0d76284be3771947b291432c18ee7e663
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2022-10-14 09:05:01 +0200

    Create tx_formpincodes_domain_model_pincodes.php

---
+++ Configuration/TCA/tx_formpincodes_domain_model_pincodes.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+    'ctrl' => [
+        'title' => 'LLL:EXT:form_pincodes/Resources/Private/Language/locallang.xlf:tx_formpincodes_domain_model_pincodes',
+        'label' => 'pincode',
+        'tstamp' => 'tstamp',
+        'crdate' => 'crdate',
+        'cruser_id' => 'cruser_id',
+        'dividers2tabs' => true,
+        'delete' => 'deleted',
+        'enablecolumns' => [
+            'disabled' => 'hidden'
+        ],
+        'searchFields' => 'pincode',
+        'iconfile' => 'EXT:form_pincodes/Resources/Public/Icons/Extension.svg'
+    ],
+    'types' => [
+        '1' => [
+            'showitem' => 'pincode, is_used'
+        ]
+    ],
+    'columns' => [
+        'pincode' => [
+            'exclude' => 1,
+            'label' => 'LLL:EXT:form_pincodes/Resources/Private/Language/locallang.xlf:tx_formpincodes_domain_model_pincodes.pincode',
+            'config' => [
+                'type' => 'input',
+                'size' => '30',
+                'readOnly' => 0
+            ]
+        ],
+        'is_used' => [
+            'exclude' => 1,
+            'label' => 'LLL:EXT:form_pincodes/Resources/Private/Language/locallang.xlf:tx_formpincodes_domain_model_pincodes.is_used',
+            'config' => [
+                'type' => 'check',
+                'readOnly' => 0
+            ]
+        ],
+    ]
+];
