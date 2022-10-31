e73e2cfe1326890b5a5479abaa954756f933325a
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2022-10-31 17:41:18 +0100

    Update ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,16 +1,34 @@
 <?php
 defined('TYPO3') or die();
 
-call_user_func(function () {
-     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
-         trim('
-             module.tx_form {
-                 settings {
-                     yamlConfigurations {
-                         1665729138 = EXT:form_pincodes/Configuration/Form/FormPincodesBackend.yaml
-                     }
-                 }
-             }
-         ')
-     );
-});
+call_user_func(
+    function ()
+    {
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
+            trim('
+                module.tx_form {
+                    settings {
+                        yamlConfigurations {
+                            1665729138 = EXT:form_pincodes/Configuration/Form/FormPincodesBackend.yaml
+                        }
+                    }
+                }
+            ')
+        );
+        
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+            'FormPincodes',
+            'Pincode',
+            [
+                \Checkitsedo\FormPincodes\Controller\PincodeController::class => 'index, edit, update'
+			],
+			// non-cacheable actions
+			[
+				\Checkitsedo\FormPincodes\Controller\PincodeController::class => 'update',
+			]
+		);
+        
+        
+        
+    }
+);
