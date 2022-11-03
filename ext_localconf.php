efca96392915dfef2027f3b3aca70c50556eaa42
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2022-11-03 09:18:49 +0100

    Update ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -38,7 +38,7 @@ call_user_func(
         ];
         // register defined icons
         $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-		foreach ($icons as $identifier => $path) {
+		foreach ($iconsToRegister as $identifier => $path) {
             $iconRegistry->registerIcon(
                 $identifier,
                 \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
