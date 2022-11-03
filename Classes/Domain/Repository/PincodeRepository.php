108044ec2586edcc5bfed4da1059b645d7e55bb8
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2022-11-03 08:47:21 +0100

    Create PincodeRepository.php

---
+++ Classes/Domain/Repository/PincodeRepository.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Checkitsedo\FormPincodes\Domain\Repository;
+
+/**
+ * This file is part of the "Form Pincodes" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * (c) 2021 Dominik Senti <info@senti.lu>, checkit senti.lu
+ */
+
+use TYPO3\CMS\Extbase\Persistence\Repository;
+
+class PincodeRepository extends Repository
+{
+
+}
